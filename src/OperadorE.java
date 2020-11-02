10c7d325f90442fd3174b54888a1ce9f98f1160f
André David <andre.david@fiap.com.br>
2020-11-02 11:04:31 -0300

    Versao base do arquivo OperadorE.java

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class OperadorE {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Faça um programa que verifique se o usuário digitado é igual a 'administrador' e se a senha digitada é igual a '123'*/
+		Scanner leitor = new Scanner(System.in);
+		String usuario, senha;
+		System.out.println("SISTEMA DE LOGIN");
+		System.out.println("Por favor, digite seu username:");
+		usuario = leitor.next();
+		System.out.println("Por favor, digite sua senha:");
+		senha = leitor.next();	
+		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
+		
+		
+		
+		leitor.close();
 
 	}
 
